01d5a75a7442753dcb63ae79e542ff6863930d83
Raul dos Santos Moraes <48498755+rauldosS@users.noreply.github.com>
2023-06-11 23:01:29 -0300

    commit

---
+++ assets/dist/custom/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active')
+    })
+
+    $("#lover-album-content").load("/templates/lover/index.html")
+})
