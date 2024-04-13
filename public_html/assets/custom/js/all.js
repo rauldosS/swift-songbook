1240374f9463010ed7a8142fab3d5162f00e8da1
Raul dos Santos Moraes <48498755+rauldosS@users.noreply.github.com>
2024-04-12 23:52:30 -0300

    Update all.js

--- public_html/assets/custom/js/all.js
+++ public_html/assets/custom/js/all.js
@@ -28,6 +28,60 @@ getLanguage = () => {
     })
 }
 
+updateLanguage = () => {
+    $('#main-alert').html(language.mainAlert)
+
+    $('#albuns .title').text(language.albuns.title)
+    $('#albuns .hide').text(language.albuns.hide)
+
+    $('#collapseSummary').html(language.summary)
+    $('#whatsapp').text(language.whatsapp)
+
+    $('#mostAccessed h4').text(language.mostAccessTitle)
+
+    $('#influencers-title').text(language.influencers.title)
+    $('#influencers-data').html(language.influencers.data)
+
+    $('#influencers-luca').text(language.influencers.luca)
+    $('#influencers-nena').text(language.influencers.nena)
+    $('#influencers-mike').text(language.influencers.mike)
+    $('#influencers-goddess').text(language.influencers.goddess)
+
+    $('#contribution-info h4').text(language.contribution.title)
+    $('#contribution-info .info-data').html(language.contribution.info)
+    $('#qr-data h3').text(language.contribution.paymant.title)
+    $('#qr-data p').text(language.contribution.paymant.info)
+    $('#qr-data #copy-code').html(language.contribution.paymant.qrCode)
+    $('#key-data p').html(language.contribution.paymant.pixKeyTitle)
+    $('#copy-key').html(language.contribution.paymant.pixKey)
+
+    $('#menu-actions .font-action-title').text(language.toolbar.fontActionTitle)
+    $('#menu-actions .chords-action-title').text(language.toolbar.chordsActionTitle)
+    $('#menu-actions .columns-action-title').text(language.toolbar.columnsActionTitle)
+
+    $('#unpin-toolbar span').text(language.toolbar.unpinToolbar)
+    $('#only-lyrics span').text(language.toolbar.onlyLyrics)
+    $('#hide-lyrics span').text(language.toolbar.hideLyrics)
+    $('#hide-progressions span').text(language.toolbar.hideProgressions)
+    $('#hide-tabs span').text(language.toolbar.hideTabs)
+    $('#hide-chords span').text(language.toolbar.hideChords)
+    $('#hide-miniplayer span').text(language.toolbar.hideMiniplayer)
+    $('#share-cipher span').text(language.toolbar.shareCipher)
+
+    $('#shortcuts #btn-home span').text(language.shortcuts.btnHome)
+    $('#shortcuts #dropdown-menu-languages').attr('data-bs-original-title', language.shortcuts.dropdownMenuLanguages)
+    $('#shortcuts #instructions').attr('data-bs-original-title', language.shortcuts.instructions)
+    $('#shortcuts #unpin-shortcuts').attr('data-bs-original-title', language.shortcuts.unpinShortcuts)
+    $('#shortcuts #pt-BR').text(language.shortcuts.languages.ptBr)
+    $('#shortcuts #en-US').text(language.shortcuts.languages.enUs)
+
+    $('.list-unstyled .about').text(language.albuns.about)
+
+    $('#search-not-found').text(language.search.searchNotFound)
+
+    changeSidebarTitle()
+}
+
 getLanguage()
 
 function setLanguage(lang) {
@@ -551,60 +605,6 @@ loadCopy = () => {
 //         loadAlbum(params.album)
 //     }
 // }
-
-updateLanguage = () => {
-    $('#main-alert').html(language.mainAlert)
-
-    $('#albuns .title').text(language.albuns.title)
-    $('#albuns .hide').text(language.albuns.hide)
-
-    $('#collapseSummary').html(language.summary)
-    $('#whatsapp').text(language.whatsapp)
-
-    $('#mostAccessed h4').text(language.mostAccessTitle)
-
-    $('#influencers-title').text(language.influencers.title)
-    $('#influencers-data').html(language.influencers.data)
-
-    $('#influencers-luca').text(language.influencers.luca)
-    $('#influencers-nena').text(language.influencers.nena)
-    $('#influencers-mike').text(language.influencers.mike)
-    $('#influencers-goddess').text(language.influencers.goddess)
-
-    $('#contribution-info h4').text(language.contribution.title)
-    $('#contribution-info .info-data').html(language.contribution.info)
-    $('#qr-data h3').text(language.contribution.paymant.title)
-    $('#qr-data p').text(language.contribution.paymant.info)
-    $('#qr-data #copy-code').html(language.contribution.paymant.qrCode)
-    $('#key-data p').html(language.contribution.paymant.pixKeyTitle)
-    $('#copy-key').html(language.contribution.paymant.pixKey)
-
-    $('#menu-actions .font-action-title').text(language.toolbar.fontActionTitle)
-    $('#menu-actions .chords-action-title').text(language.toolbar.chordsActionTitle)
-    $('#menu-actions .columns-action-title').text(language.toolbar.columnsActionTitle)
-
-    $('#unpin-toolbar span').text(language.toolbar.unpinToolbar)
-    $('#only-lyrics span').text(language.toolbar.onlyLyrics)
-    $('#hide-lyrics span').text(language.toolbar.hideLyrics)
-    $('#hide-progressions span').text(language.toolbar.hideProgressions)
-    $('#hide-tabs span').text(language.toolbar.hideTabs)
-    $('#hide-chords span').text(language.toolbar.hideChords)
-    $('#hide-miniplayer span').text(language.toolbar.hideMiniplayer)
-    $('#share-cipher span').text(language.toolbar.shareCipher)
-
-    $('#shortcuts #btn-home span').text(language.shortcuts.btnHome)
-    $('#shortcuts #dropdown-menu-languages').attr('data-bs-original-title', language.shortcuts.dropdownMenuLanguages)
-    $('#shortcuts #instructions').attr('data-bs-original-title', language.shortcuts.instructions)
-    $('#shortcuts #unpin-shortcuts').attr('data-bs-original-title', language.shortcuts.unpinShortcuts)
-    $('#shortcuts #pt-BR').text(language.shortcuts.languages.ptBr)
-    $('#shortcuts #en-US').text(language.shortcuts.languages.enUs)
-
-    $('.list-unstyled .about').text(language.albuns.about)
-
-    $('#search-not-found').text(language.search.searchNotFound)
-
-    changeSidebarTitle()
-}
 
 // loadMusic = music => {
 //     try {
